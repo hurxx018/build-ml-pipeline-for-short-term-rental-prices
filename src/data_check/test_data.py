f5527e4014e4586a2d898138b51de971ae640b87
Kwang Ho Hur <hurxx018@gmail.com>
2022-12-16 07:48:10 -0800

    update test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,18 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+
+def test_row_count(data: pd.DataFrame):
+    """
+    Test the number of row is in the range
+    """
+
+    assert 15000 < len(data) < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test values of min_price are within the range (min_price, max_price)
+    """
+
+    assert data["price"].between(min_price, max_price)
